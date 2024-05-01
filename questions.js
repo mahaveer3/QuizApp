ae2240b96bb4981c80521887e90ac209618f45e0
mahaveer3 <work.mahaveerkankaria@gmail.com>
2024-05-01 23:20:29 +0530

    Initial commit

---
+++ questions.js
@@ -0,0 +1,57 @@
+let questions = [
+    {
+        numb: 1,
+        question: "In socket programming, what does the getInetAddress() method in the Socket class return?",
+        answer: "C. The client's remote address",
+        options: [
+            "A.The client's local address",
+            "B.The server's local address",
+            "C. The client's remote address",
+            "D.The server's remote address"
+        ]
+    },
+    {
+        numb: 2,
+        question: "In socket programming, what does the term binding refer to?",
+        answer: "B.Assigning a port number and IP address to a socket",
+        options: [
+            "A.Establishing a connection with the server",
+            "B.Assigning a port number and IP address to a socket",
+            "C.Transmitting data between client and server",
+            "D.Closing the socket connection"
+        ]
+    },
+    {
+        numb: 3,
+        question: "What does PHP stand for?",
+        answer: "A. Hypertext Preprocessor",
+        options: [
+            "A. Hypertext Preprocessor",
+            "B. Hometext Programming",
+            "C. Hypertext Preprogramming",
+            "D. Programming Hypertext Preprocessor"
+        ]
+    },
+    {
+        numb: 4,
+        question: "What does SQL stand for?",
+        answer: "D. Structured Query Language",
+        options: [
+            "A. Strength Query Language",
+            "B. Stylesheet Query Language",
+            "C. Science Question Language",
+            "D. Structured Query Language"
+        ]
+    },
+    {
+        numb: 5,
+        question: "What does XML stand for?",
+        answer: "D. Extensible Markup Language",
+        options: [
+            "A. Excellent Multiple Language",
+            "B. Explore Multiple Language",
+            "C. Extra Markup Language",
+            "D. Extensible Markup Language"
+        ]
+    }
+];
